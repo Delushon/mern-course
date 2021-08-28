0cbc2ff1e28583946892572faa9006b5da918955
Alexander P <panshin.alexander@yahoo.com>
2021-08-29 01:07:15 +0300

    hook

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,6 +1,16 @@
-import React from "react"
+import React, {useState} from "react"
 
 export const AuthPage = () => {
+    const [form, setForm] = useState({
+        email: '', password: ''
+    })
+
+    const changeHandler = event => {
+        setForm({ ...form, [event.target.name]: event.target.value })
+    }
+
+
+
     return (
         <div className="row">
             <div className="col s6 offset-s3">
@@ -9,6 +19,26 @@ export const AuthPage = () => {
                     <div className="card-content white-text">
                         <span className="card-title">Авторизация</span>
                         <div>
+
+                            <div className="input-field">
+                                <input placeholder="Введите email"
+                                       id="email"
+                                       type="text"
+                                       name="email"
+                                       className="yellow-input"
+                                       onChange={changeHandler}/>
+                                    <label htmlFor="email">Email</label>
+                            </div>
+
+                            <div className="input-field">
+                                <input placeholder="Введите пароль"
+                                       id="password"
+                                       type="password"
+                                       name="password"
+                                       className="yellow-input"
+                                       onChange={changeHandler}/>
+                                <label htmlFor="password">Пароль</label>
+                            </div>
 
                         </div>
                     </div>
