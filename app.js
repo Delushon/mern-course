5249a6abc978973333ca86c2bf9b6e192d2726ff
Alexander P <panshin.alexander@yahoo.com>
2021-08-28 21:34:50 +0300

    Подключение к БД, конфиги

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+
+const app = express()
+
+
+
+const PORT = config.get('port') || 5000
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        app.listen(PORT, () => console.log(`LISTEN PORT: ${PORT}`))
+    } catch (e) {
+        console.log('ERROR', e.message)
+        process.exit()
+    }
+}
+
+start()
+
