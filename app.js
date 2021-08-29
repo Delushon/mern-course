40d2032eacd6b2a99b874031eb00688c5661201e
Alexander P <panshin.alexander@yahoo.com>
2021-08-29 20:56:52 +0300

    готовый сервис сокращения ссылок, подсчета перехода по ним

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(express.json({ extended: true }))
 
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/link', require('./routes/link.routes'))
+app.use('/t', require('./routes/redirect.routes'))
 
 const PORT = config.get('port') || 5000
 
